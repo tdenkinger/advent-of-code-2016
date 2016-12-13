12ad5b12341e8e18df66c8d966f0c280bc8c46e6
Troy Denkinger <tdenkinger@gmail.com>
2016-12-13 08:43:26 -0500

    Segregate the input data by day.

--- test/day1_test.exs
+++ test/day1_test.exs
@@ -3,11 +3,11 @@ defmodule Advent.Day1Test do
   alias Advent.Day1
 
   test "Gets the distance from the origin" do
-    assert Day1.calculate("n", [{0, 0}], Advent.data) == 300
+    assert Day1.calculate("n", [{0, 0}], Advent.Data.day1) == 300
   end
 
   test "Gets the coordinates first revisted" do
-    assert Day1.find_revisit("n", [{0, 0}], Advent.data) == 159
+    assert Day1.find_revisit("n", [{0, 0}], Advent.Data.day1) == 159
   end
 end
 
